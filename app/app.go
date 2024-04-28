d927a7db0b5f57a27e0ac8d64bf42eb9f5b21324
lihua <lihua@zkme.xyz>
2024-04-28 12:03:41 +0800

    ci:git review

--- app/app.go
+++ app/app.go
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with the Evmos packages. If not, see https://github.com/evmos/evmos/blob/main/LICENSE
-// e2e
+// e2e ccc
 package app
 
 import (
